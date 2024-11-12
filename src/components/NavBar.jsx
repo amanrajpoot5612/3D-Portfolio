87118ddf5d103e7e91e632b18f9effc0f946f93f
Aman Rajpoot <amanrajpoot5612@gmail.com>
2024-11-12 22:14:27 +0530

    Add NavBar and Sidebar components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div className='NavBar'>
+            <nav>
+            <ul>
+                <li><a href="#" >Projects</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Contact Me</a></li>
+            </ul>
+        </nav>
+        </div>
+    )
+}
+
+export default NavBar;
